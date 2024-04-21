2e4da3969387722b3090bb9d3e62c8f64280ef6e
Moffein <22742925+Moffein@users.noreply.github.com>
2024-04-21 01:01:02 -0700

    Glass now attempts to use hopoo glass material

--- RiskyClassicItemsSolution/Items/ItemBase.cs
+++ RiskyClassicItemsSolution/Items/ItemBase.cs
@@ -380,6 +380,7 @@ namespace ClassicItemsReturns.Items
             return spr;
         }
 
+        private static Material hopooGlassMat = Addressables.LoadAssetAsync<Material>("RoR2/Base/Infusion/matInfusionGlass.mat").WaitForCompletion();
         public GameObject LoadItemModel(string modelName)
         {
             GameObject mdl3d = Assets.LoadObject("mdl3d" + modelName);
@@ -397,7 +398,7 @@ namespace ClassicItemsReturns.Items
                             if (mesh.name == "UseGlassShader")
                             {
                                 Debug.Log("ClassicItemsReturns: Swapping shader to Glass Shader");
-                                mesh.material.shader = Addressables.LoadAssetAsync<Shader>("RoR2/Base/Shaders/HGIntersectionCloudRemap.shader").WaitForCompletion();
+                                mesh.material = hopooGlassMat;
                             }
                             else
                             {
