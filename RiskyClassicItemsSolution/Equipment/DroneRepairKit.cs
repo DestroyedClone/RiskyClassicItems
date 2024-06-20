218438f0e08d7a194534168ef6c5672a8d5ae156
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-06-20 10:03:10 -0700

    Removed drone spawning stuff because delegation

--- RiskyClassicItemsSolution/Equipment/DroneRepairKit.cs
+++ RiskyClassicItemsSolution/Equipment/DroneRepairKit.cs
@@ -32,21 +32,9 @@ namespace ClassicItemsReturns.Equipment
         public static BuffDef DroneRepairBuff => Buffs.DroneRepairBuff;
         public override bool Unfinished => true;
 
-        public static GameObject droneBody;
-        public static GameObject droneMaster;
-        public static CharacterSpawnCard droneSpawnCard;
-
         public override void CreateAssets(ConfigFile config)
         {
             base.CreateAssets(config);
-            //DRK = DroneRepairKit
-            droneBody = PrefabAPI.InstantiateClone(UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Drones/BackupDroneBody.prefab").WaitForCompletion(), "CIR_DRKDroneBody");
-            var body = droneBody.GetComponent<CharacterBody>();
-            body.baseNameToken = "CLASSICITEMSRETURNS_DRKDRONE_BODY_NAME";
-            body.subtitleNameToken = "CLASSICITEMSRETURNS_DRKDRONE_BODY_SUBTITLE";
-
-            droneMaster = PrefabAPI.InstantiateClone(UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Drones/DroneBackupMaster.prefab").WaitForCompletion(), "CIR_DRKDroneMaster");
-            droneMaster.GetComponent<CharacterMaster>().bodyPrefab = droneBody;
         }
 
         public override void Hooks()
@@ -72,24 +60,6 @@ namespace ClassicItemsReturns.Equipment
 
         protected override bool ActivateEquipment(EquipmentSlot slot)
         {
-            int sliceCount = 1;
-            float num = 25f;
-            if (NetworkServer.active) //todo: remove slice code
-            {
-                float y = Quaternion.LookRotation(slot.GetAimRay().direction).eulerAngles.y;
-                float d = 3f;
-                foreach (float num2 in new DegreeSlices(sliceCount, 0.5f))
-                {
-                    Quaternion rotation = Quaternion.Euler(-30f, y + num2, 0f);
-                    Quaternion rotation2 = Quaternion.Euler(0f, y + num2 + 180f, 0f);
-                    Vector3 position = slot.transform.position + rotation * (Vector3.forward * d);
-                    CharacterMaster characterMaster = slot.SummonMaster(droneMaster, position, rotation2);
-                    if (characterMaster)
-                    {
-                        characterMaster.gameObject.AddComponent<MasterSuicideOnTimer>().lifeTimer = num + UnityEngine.Random.Range(0f, 3f);
-                    }
-                }
-            }
             int activationCount = 0;
             foreach (var mechAlly in CharacterBody.readOnlyInstancesList)
             {
